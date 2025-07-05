f469fd28b138cbd5b2b5ff3c637b0db19918ed1f
slogacor <134832245+slogacor@users.noreply.github.com>
2025-07-06 00:44:48 +0700

    Update main.py

--- main.py
+++ main.py
@@ -258,5 +258,8 @@ async def main():
 if __name__ == "__main__":
     import asyncio
     import nest_asyncio
+
     nest_asyncio.apply()
-    asyncio.run(main())
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main())
+
